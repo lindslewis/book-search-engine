84ac6fce952fb28ef315d7342dbecdd203771cb3
LindsayLew <lmschwehr@gmail.com>
2022-09-05 13:11:13 -0700

    resolvers start

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,1 +1,10 @@
 // hint in readme says to use the functionality in user-controller.js as a guide....
+
+// these are both called at the top of user-controller
+const { User } = require('../models');
+const { signToken } = require('../utils/auth');
+
+
+const resolvers = {
+
+}
