eb867a4ac3f5d4690914306cc7fdacb3a3c6bf31
LindsayLew <lmschwehr@gmail.com>
2022-09-05 11:50:16 -0700

    some psuedo coding for type def

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,18 @@
+const gql = require('apollo-server-express');
+
+// need to define:
+// query type as me: returning user type
+
+
+// mutation type as login: accepting email password as parameters. returns an Auth type
+// mutation cont
+// addUser accepts a username, email and password as params, returning Auth type
+// saveBook accepts a book author's array, description, title, bookId, image, and link as params. returns a User type
+// hint for the saveBook is to look into making an input type to handle all of the params
+// removeBook, accepts book's bookId as param, returning user type
+
+// user type takes in _id, username, email, bookCount, savedbooks (which is an array of book type)
+
+// book type takes in bookId (not _id, but books id returned from google books api), authors (array of strings), description, title, image, link
+
+// auth type takes in token and user (referencing user type)
